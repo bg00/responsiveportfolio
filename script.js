ede595264ea5c1afa5244057b188bbac22051b7c
Bailey Garrett <baileyagarrett@gmail.com>
2020-02-19 19:09:49 -0600

    adding logos w/ js file links for portfolio page, updated git ignore

---
+++ script.js
@@ -0,0 +1,23 @@
+$('.isslogo').on('click', function() {
+  window.open("https://bg00.github.io/ISS-Tracker/");
+});
+
+$(".fwflogo").on("click", function() {
+  window.open("https://github.com/bg00/project2");
+});
+
+$(".sweapp").on("click", function() {
+  window.open("https://young-crag-30640.herokuapp.com/");
+});
+
+$(".weatherdashboard").on("click", function() {
+  window.open("https://bg00.github.io/weatherdashboard/");
+});
+
+$(".notetaker").on("click", function() {
+  window.open("https://bg00.github.io/notetaker/");
+});
+
+$(".burger").on("click", function() {
+  window.open("http://www.google.com/");
+});
